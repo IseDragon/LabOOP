df28813abae32a97def3fe7bac80adff9665e0e1
IseDragon <tourer-ss@yandex.ru>
2017-12-10 11:25:50 +0700

    Add New func

--- Film.h
+++ Film.h
@@ -16,5 +16,6 @@ public:
 	virtual void InData(ifstream &ifst) = 0; // ввод
 	virtual void Out(ofstream &ofst) = 0;    // вывод
 	int SumElementsOfString(int n);
+	int FuncFilm();
 };
 #endif
---
+++ FuncFilm.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include "Film.h"
+
+using namespace std;
+
+int film::FuncFilm() {
+	char glasnie[] = "aeiouAEIOUàóîûèıÿş¸åÀÓÎÛÈİßŞ¨Å";
+	int kol = 0;
+	for (int i = 0; i < strlen(name); ++i) {
+		if (strchr(glasnie, name[i])) {
+			++kol;
+		}
+	}
+	return kol;
+}
